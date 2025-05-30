8626da7199658d3174a47a6607b6982dc086715d
Aboubacar Kamara <aboubacarkamara31@gmail.com>
2025-05-30 20:31:25 +0200

    correction

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = FastAPI()
 with open("data/vectorstore.pkl", "rb") as f:
     vectorstore = pickle.load(f)
 
+
 @app.get("/")
 def root():
     return {"message": "API en ligne ✅"}
@@ -17,5 +18,9 @@ def root():
 async def ask(request: Request):
     body = await request.json()
     question = body.get("question")
+    if not question:
+        return {"error": "Aucune question fournie."}
+    print(f"❓ Question reçue : {question}")
     answer = answer_query(question, vectorstore)
+    print(f"✅ Réponse générée")
     return {"answer": answer}
