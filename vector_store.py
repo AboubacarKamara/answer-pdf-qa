ed6ad22a725785e94848d1dfe07075c28bcd18b5
Aboubacar Kamara <aboubacarkamara31@gmail.com>
2025-06-03 09:37:07 +0200

    correct search func

--- vector_store.py
+++ vector_store.py
@@ -11,20 +11,23 @@ class VectorStore:
         self.texts.extend(metadata)
     
     def search(self, query_embedding, top_k=5):
-        q_emb = np.array([query_embedding]).astype('float32')
+        q_emb = np.array([query_embedding], dtype='float32')
         n, d = q_emb.shape
+
+        n = np.int64(n)
+        top_k = np.int64(top_k)
 
         distances = np.empty((n, top_k), dtype='float32')
         labels = np.empty((n, top_k), dtype='int64')
 
-        # Appel à l'API SWIG (low-level)
         self.index.search(n, faiss.swig_ptr(q_emb), top_k,
-                          faiss.swig_ptr(distances), faiss.swig_ptr(labels))
+                        faiss.swig_ptr(distances), faiss.swig_ptr(labels))
 
         results = []
         for i in labels[0]:
             if i < len(self.texts):
                 results.append(self.texts[i])
         return results
+
 
 
